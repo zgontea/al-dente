8a52fe392e2ba750b583da86d23d6bf315079cf3
janek <janekgontarek@gmail.com>
2022-04-15 21:13:59 +0200

    created dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,12 +17,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -25,9 +34,18 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MatCardModule,
     MatInputModule,
     FormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatProgressBarModule
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface PeriodicElement {
+  time: string;
+  name: string;
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {time: '8:00 - 8:45', name: 'Fran Kar', status: 'In Progress'},
+  {time: '9:00 - 9:45', name: 'Tom Sztajka', status: 'Planned'},
+  {time: '10:00 - 10:45', name: 'Jan Gontar', status: 'Planned'},
+  {time: '11:00 - 11:45', name: 'Francisław z Bogdańca', status: 'Declined'},
+  {time: '12:00 - 12:45', name: 'Tomaszek Sztajkaszek', status: 'Planned'},
+  {time: '13:00 - 13:45', name: 'Janicja Gontaricja', status: 'Planned'},
+  {time: '14:00 - 14:45', name: 'Francich Karwa', status: 'Declined'},
+  {time: '15:00 - 15:45', name: 'Jancich Gonta', status: 'Planned'}
+];
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +25,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
+  displayedColumns: string[] = ['time', 'name', 'status', 'actions'];
+  dataSource = ELEMENT_DATA;
+
+  imageSrc = '../assets/logo.png';
+
+  selected: Date | null | undefined;
 
   constructor() { }
 
